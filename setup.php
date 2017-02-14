b3bfd4e37614251f4f2dcf7b4f74d624ee9f5fd4
Edgard Lorraine Messias <edgardmessias@gmail.com>
2017-02-13 23:19:12 -0200

    Fixed PHP Warning;

--- setup.php
+++ setup.php
@@ -1,6 +1,6 @@
 <?php
 
-define('PLUGIN_BROWSERNOTIFICATION_VERSION', '1.0.0');
+define('PLUGIN_BROWSERNOTIFICATION_VERSION', '1.0.1');
 
 // Init the hooks of the plugins -Needed
 function plugin_init_browsernotification() {
