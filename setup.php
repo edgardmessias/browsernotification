4602ce1ca14d6c64343e6f70f980884fdf004146
Edgard Lorraine Messias <edgardmessias@gmail.com>
2017-02-24 09:16:58 -0300

    New release

--- setup.php
+++ setup.php
@@ -1,6 +1,6 @@
 <?php
 
-define('PLUGIN_BROWSERNOTIFICATION_VERSION', '1.1.0');
+define('PLUGIN_BROWSERNOTIFICATION_VERSION', '1.1.1');
 
 // Init the hooks of the plugins -Needed
 function plugin_init_browsernotification() {
