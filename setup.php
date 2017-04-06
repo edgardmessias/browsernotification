0ca6066e403579b94b84f6fe6fab9d5d680c99f5
Edgard Lorraine Messias <edgardmessias@gmail.com>
2017-04-06 08:27:31 -0300

    Added Italian translation (Close #10)

--- setup.php
+++ setup.php
@@ -1,6 +1,6 @@
 <?php
 
-define('PLUGIN_BROWSERNOTIFICATION_VERSION', '1.1.6');
+define('PLUGIN_BROWSERNOTIFICATION_VERSION', '1.1.7');
 
 // Init the hooks of the plugins -Needed
 function plugin_init_browsernotification() {
