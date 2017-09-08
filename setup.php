9271faa18e377907f30db650942a79dbea3af7d5
Edgard Lorraine Messias <edgardmessias@gmail.com>
2017-09-08 09:15:38 -0300

    New release 1.1.9

--- setup.php
+++ setup.php
@@ -1,6 +1,6 @@
 <?php
 
-define('PLUGIN_BROWSERNOTIFICATION_VERSION', '1.1.8');
+define('PLUGIN_BROWSERNOTIFICATION_VERSION', '1.1.9');
 
 // Init the hooks of the plugins -Needed
 function plugin_init_browsernotification() {
